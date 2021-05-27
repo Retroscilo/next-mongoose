8f4220616dfe4a57d01340f6bdf47f407cb9bba5
Loïc Torres <loic.torresg@gmail.com>
2021-05-27 18:56:53 +0200

    updated path

--- public/productLabels/index.js
+++ public/productLabels/index.js
@@ -1,4 +1,4 @@
-export { default as HomeMade } from './Homemade'
+export { default as HomeMade } from './HomeMade'
 export { default as GlutenFree } from './GlutenFree'
 export { default as Spicy } from './Spicy'
 export { default as Vegan } from './Vegan'
